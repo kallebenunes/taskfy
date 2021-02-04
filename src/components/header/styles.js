7edad0700ef5ec836fdc0067331afe307f90c549
Kallebe Nunes <kallebenm@gmail.com>
2021-02-04 20:43:00 -0300

    add react-icons

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-    height: 80px; 
+    height: 7%; 
     padding: 0 30px; 
     background: #7159c1; 
     color: #fff; 
--- src/styles/global.js
+++ src/styles/global.js
@@ -3,6 +3,9 @@ import {createGlobalStyle} from 'styled-components'
 export default createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
 
+    :root {
+        font-size: 10px;
+    }
 
     * {
         margin: 0; 
@@ -16,7 +19,7 @@ export default createGlobalStyle`
     }
 
     body {
-        font: 14px 'Robot', sans-serif; 
+        font-family:  'Robot', sans-serif; 
         background: #ecf1f8;
         color: #333;
     }
